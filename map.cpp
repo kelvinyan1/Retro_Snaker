d95868ddef9fe4728ac2dbeed983d4d4e3845440
kelvinyan1 <kelvinyzp@gmail.com>
2020-04-27 16:36:57 +0800

    Update map.cpp
    append print map function

--- map.cpp
+++ map.cpp
@@ -1,4 +1,5 @@
-int map[30][30];
+#include <iostream>
+using namespace std;
 
 void init_map(int map[30][30]){
     for(int i=0;i<30;++i){
@@ -10,5 +11,13 @@ void init_map(int map[30][30]){
                 map[i][j]=0;
             }
         }
+    }
+}
+void print_map(int map[30][30]){
+    for(int i=0;i<30;++i){
+        for(int j=0;j<30;++j){
+            cout << map[i][j];
+        }
+        cout<<endl;
     }
 }
