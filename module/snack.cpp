2750c89e5c7ef03d9601c7a89e91a4f9844b19bf
kelvinyan1 <kelvinyzp@gmail.com>
2020-04-29 23:26:05 +0800

    Append snack class

--- module/snack.cpp
+++ module/snack.cpp
@@ -1,5 +1,39 @@
 #include <snack.h>
-void snack_init(){
+void snack::snack_init(){
     
 }
-void snack_move()
+void snack::snack_move(char input,int * flag_ptr)
+{
+    /*to be finished*/
+}
+
+void snack::print_snack(int map[30][30])
+   {
+       body* current=snack_head;
+    while(current !=snack_tail)
+    {
+            map[current->body_x][current->body_y]='&';
+        current = current->next;
+    }
+    map[current->body_x][current->body_y]='&';
+}
+
+int snack::snack_dead()       //return 1 if snack is dead, 0 otherwise
+{   
+    body* current = snack_head;
+    while(current != snack_tail)
+    {
+        if ((current->body_x==snack_head->body_x) &&(current->body_y==snack_head->body_y))
+        {
+           return 1;
+        }
+        current = current->next;
+    }
+    if ((current->body_x==snack_head->body_x) &&(current->body_y==snack_head->body_y))
+    {
+         return 1;
+    }
+    else{
+         return 0;
+    }
+}
--- module/snack.h
+++ module/snack.h
@@ -1,26 +1,37 @@
 #ifndef _SNACK_H
 #define _SNACK_H
 
-struct body{
-    int body_x;
-    int body_y;
-    body* next;
-    body* previous;      
-};
+class snack {
+public:
+    struct body
+    {        
+        int body_x;
+        int body_y;
+        body* next;
+        body* previous;      
+    };
+    /*up = 0, down = 1, left = 2, right = 3*/
+    enum direction
+    {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    };
+    
+    body* snack_tail;
+    body* snack_head;
+    
+    /*return 1 if snack is dead, 0 otherwise*/
+    int snack_dead();      
+    
+    /*move and make the snack longer*/
+    void snack_move(char ,int *);
 
-enum direction
-{
-    UP,
-    DOWN,
-    LEFT,
-    RIGHT
-};
+    /*append snack into map*/
+    void print_snack(int [30][30]);
 
-struct snake{
-    int length;
-    direction dir;//0 for up, 1 for down, 2 for left, 3 for right
-    body* head;
+    void snack_init();
 };
-
 
 #endif
