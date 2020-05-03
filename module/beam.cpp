5597455de7f9cf07ec3bb098731faedbc5cbbd89
kelvinyan1 <kelvinyzp@gmail.com>
2020-05-03 17:02:47 +0800

    modify beam_generate

--- module/beam.cpp
+++ module/beam.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int beam::beam_generate()
 {
     srand(time(NULL)); // randomly generate the position of the beam
-    x = rand() % 30;
-    y = rand() % 30;
+    x = rand() % 28 +1;
+    y = rand() % 28 +1;
 }
 
 int beam::beam_print(char map[30][30])
